65ae8671483a5393db6ab1bf993436b5ce720fc1
Lwalkl <lwlavengood@gmail.com>
2024-05-24 11:55:04 -0400

    Adding Comments

--- post-processing/HydroSurveyor/mat_readin.py
+++ post-processing/HydroSurveyor/mat_readin.py
@@ -1,32 +1,32 @@
-import numpy as np 
 from scipy.io import loadmat
 import pandas as pd
-import datetime as time
-import pytest
-import matplotlib.pyplot as plt
 
 #Import .mat files 
 def readin(filepath) :
-        rawdata = loadmat(filepath)
-        del rawdata['__header__'] ; del rawdata['__version__'] ; del rawdata['__globals__']; #Delete unnecessary keys
-        data = {}
-        for i in rawdata.keys() :
-                array = rawdata[i]
-                if rawdata[i].ndim == 3 :      #Flatten 3d arrays into 2d arrays, this causes 
+        rawdata = loadmat(filepath)            #Load in filepath, make sure you put an r out front r"placeholder/placeholder/placeholder.mat" so python
+                                               # interprets the backslashes correctly
+        del rawdata['__header__'] ; del rawdata['__version__'] ; del rawdata['__globals__']; #Delete unnecessary keys that list info about the function
+        data = {} #Initialize data dictionary to place data into 
+        for i in rawdata.keys() : #Run through loop where i is the key to the array in the dictionary
+                array = rawdata[i]  #Initialize a a duplicate array to allow for reshaping 
+                if rawdata[i].ndim == 3 :      #Flatten 3d arrays into 2d arrays, this causes an 3577x85x4 array to become 3577x340
                         reshaped = array.reshape(array.shape[0],-1)   
-                        data[i] = pd.DataFrame(reshaped)
-                elif i == 'Info':              #The info structure does not translate well but contains relevant data
-                        Info = rawdata[i]
-                else :
+                        data[i] = pd.DataFrame(reshaped)       #Save the data into the dictionary as a dataframe, dataframes are one of the many ways to 
+                                                               #store data in python
+                elif i == 'Info':              #The info structure does not transfer well between MATLAB and Python but contains relevant data so we just 
+                                               #make a 1 to 1 copy
+                        Info = rawdata[i]       
+                else :                         #All 2d arrays get stored as a dataframe, this is the majority of data arrays in the structure
                         data[i] = pd.DataFrame(array)
         return data , Info
 
-def mat_readin(filepath) :
+#Function that uses the previous readin function to acquire the data, and then any further processing to store the variables in a favorable way 
+def mat_readin(filepath) :                     
         data, Info = readin(filepath)
         #Acquire individual direction dataframes
-        WaterEastVel = data['WaterVelEnu_m_s'].iloc[:, 0::4] #3557x340 matrix gets sorted for every fourth column 
+        WaterEastVel = data['WaterVelEnu_m_s'].iloc[:, 0::4] #3557x340 matrix gets sorted for every fourth column, 
+        WaterVertVel = data['WaterVelEnu_m_s'].iloc[:, 2::4] #this pulls out the individual ENU and Error columns for each of the 85 cells
         WaterNorthVel = data['WaterVelEnu_m_s'].iloc[:,1::4] 
-        WaterVertVel = data['WaterVelEnu_m_s'].iloc[:, 2::4] 
         WaterErrVel = data['WaterVelEnu_m_s'].iloc[:, 3::4] 
         rawdata = data
         return rawdata, WaterEastVel, WaterNorthVel, WaterVertVel, WaterErrVel
