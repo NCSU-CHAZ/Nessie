4d2c7f9c376fd0868e0ff19f374ea1c00e76bbcf
lexijacobs7 <aijacobs@ncsu.edu>
2025-06-09 20:59:36 +0000

    filepath/shoreline_orientation variables created for user input into Hydro_process

--- copy_FPS.py
+++ copy_FPS.py
@@ -15,20 +15,17 @@ from rasterio.transform import rowcol
 # script then loads the data using pickle and generates some plots. Some of the plots require other individual data streams to work.
 # To get these other data streams simple run hydroprocess on the raw data for whatever it needs to process.
 
+filepath = r"D:\Research\Hydro-JFE\Hydro_March\1_M9Hydro_Corrected.mat"
 interpsize = 2  # This would be .05m for the interpolated cell size
+shoreline_orientation = 112
 
-# CombinedData = Hydro_process(
-#     r"Z:\BHI_NearshoreJetskiSurvey_Data\2025_05_01\2025_05_01_BHI_unprocessed.mat",
-#     interpsize,
-# )
+Data = Hydro_process(filepath, interpsize, shoreline_orientation)
 # with open(
 #     r"Z:\BHI_NearshoreJetskiSurvey_Data\2025_05_01\2025_05_01_processed.txt", "wb"
 # ) as file:
 #     pickle.dump(CombinedData, file)
 
-with open(
-    r"D:\Research\Hydro-JFE\Hydro_March\1_M9Hydro_Corrected.mat",
-    "rb",
+with open(Data, "rb",
 ) as file:
     CombinedData = pickle.load(file)
 
